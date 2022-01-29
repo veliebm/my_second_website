1d9a7197fc35b29c76de951d668154303eb81c9d
Ben Velie <67563877+veliebm@users.noreply.github.com>
2022-01-29 08:43:28 -0500

    Add unsecure, basic login page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the Flask class from the flask module.
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # Create the application object.
 app = Flask(__name__)
@@ -12,6 +12,16 @@ def home():
 @app.route("/welcome")
 def welcome():
     return render_template("welcome.html")  # Render a template.
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid Credentials. Please try again."
+        else:
+            return redirect(url_for("home"))
+    return render_template("login.html", error=error)
 
 # Start the server with the "run()" method.
 if __name__ == "__main__":
