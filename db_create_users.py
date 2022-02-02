aba222391cfca04a510cd89160b5db5b5c0e4e40
Ben <veliebm@gmail.com>
2022-02-02 18:11:14 -0500

    Add script to add teest users to database

---
+++ db_create_users.py
@@ -0,0 +1,13 @@
+from app import db
+
+from models import User
+
+# Create the database and the db tables.
+db.create_all()
+
+# Insert.
+db.session.add(User("ben", "ben@ben.com", "sekrit"))
+db.session.add(User("admin", "admin@admin.com", "admin"))
+
+# Commit the changes.
+db.session.commit()
