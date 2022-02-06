8955a66f0bef0c02a577f3bdadbf67ce12ff3bff
Ben <veliebm@gmail.com>
2022-02-06 08:31:26 -0500

    Modularize tests

--- manage.py
+++ manage.py
@@ -17,16 +17,16 @@ manager.add_command("db", MigrateCommand)
 @manager.command
 def test():
     """Runs the unit tests without coverage."""
-    tests = unittest.TestLoader().discover(".")
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 @manager.command
 def cov():
     """Runs the unit tests with coverage."""
-    cov = coverage.coverage(branch=True, include="project/*")
+    cov = coverage.coverage(branch=True, include="project/*", omit="*/__init__.py")
     cov.start()
-    tests = unittest.TestLoader().discover(".")
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
     cov.stop()
     cov.save()
