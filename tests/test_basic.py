8d263c1a44c856a76192993557b8cef6ecc320f2
Ben <veliebm@gmail.com>
2022-02-08 15:57:44 -0500

    Make comments slightly prettier

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,3 @@
-# tests/test_basic.py
-
 import unittest
 
 from base import BaseTestCase
@@ -7,22 +5,22 @@ from base import BaseTestCase
 
 class FlaskTestCase(BaseTestCase):
 
-    # Ensure that Flask was set up correctly
+    # Ensure that Flask was set up correctly.
     def test_index(self):
         response = self.client.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    # Ensure that main page requires user login
+    # Ensure that main page requires user login.
     def test_main_route_requires_login(self):
         response = self.client.get('/', follow_redirects=True)
         self.assertIn(b'Please log in to access this page', response.data)
 
-    # Ensure that welcome page loads
+    # Ensure that welcome page loads.
     def test_welcome_route_works_as_expected(self):
         response = self.client.get('/welcome', follow_redirects=True)
         self.assertIn(b'Welcome to Flask!', response.data)
 
-    # Ensure that posts show up on the main page
+    # Ensure that posts show up on the main page.
     def test_posts_show_up_on_main_page(self):
         response = self.client.post(
             '/login',
